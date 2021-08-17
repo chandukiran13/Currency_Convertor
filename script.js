ef304cfbfb65e11da6f73ef78301105552739deb
Chandu Kiran <chandukiran13@gmail.com>
2021-08-17 11:15:02 +0530

    Implemented Api

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-function convert() {
-    let fromval = document.getElementById("FromVal");
-    let toVal = document.getElementById("ToVal");
-    let fro = document.getElementById("FromDrop");
-    let tot = document.getElementById("ToDrop");
+let fromval = document.getElementById("FromVal");
+let toVal = document.getElementById("ToVal");
+let fro = document.getElementById("FromDrop");
+let tot = document.getElementById("ToDrop");
+let dropdownFrom = document.getElementById("FromDrop");
+let dropdownTo = document.getElementById("ToDrop");
+let api = "https://api.exchangerate-api.com/v4/latest/USD";
+const currencies = new Set(["USD","EUR","JPY","GBP","AUD","CAD","CHF","CNY","PKR","USD","SAR","AED","INR","EUR","RUB","GBP","CNY","JPY","CAD","AUD","SDG","IDR","SGD","ZAR","KWD","TRY","OMR","HKD","AFN","MYR","VND","JMD","IRR","NZD","BND","QAR","CHF",'MXN']);
+for (const k of currencies.keys()){
+    dropdownFrom.innerHTML+= `<option value="${k}">${k}</option>`;
+    dropdownTo.innerHTML+= `<option value="${k}">${k}</option>`
+}
+function fetchValue(){
+    fetch(api)
+            .then(function (respose) {
+                let data = respose.json();
+                return data;
+            })
+            .then(function (data) {
+                localStorage.setItem("currencyValue",JSON.stringify(data["rates"]));
+                console.log("I'm in fetching")
+                convert();
+            })
+
+}
+function convert(){
     let giveVal = fromval.value;
     let fromCurrency = fro.value;
     let toCurrency = tot.value;
-    console.log(fromCurrency);
-    if(fromCurrency === "INR" && toCurrency ==="AUS"){
-        toVal.value = 0.018*giveVal;
+    if(localStorage.getItem("currencyValue")===null){
+        fetchValue();
     }
-    if(fromCurrency === "INR" && toCurrency ==="INR"){
-        toVal.value = giveVal;
-    }else if(fromCurrency === "INR" && toCurrency ==="USD"){
-        toVal.value = 0.013*giveVal;
-    }else if(fromCurrency === "USD" && toCurrency ==="AUS"){
-        toVal.value = 1.36*giveVal;
-    }else if(fromCurrency === "AUS" && toCurrency ==="AUS"){
-        toVal.value = giveVal;
-    }else if(fromCurrency === "AUS" && toCurrency ==="INR"){
-        toVal.value = 54.69*giveVal;
-    }else if(fromCurrency === "AUS" && toCurrency ==="USD"){
-        toVal.value = 0.74*giveVal;
-    }else if(fromCurrency === "USD" && toCurrency ==="INR"){
-        toVal.value = 74.22*giveVal;
-    }else if(fromCurrency  === "USD" && toCurrency === "USD"){
-        toVal.value = giveVal;
+    else{
+        console.log("I'm here");
+        var conversion = localStorage.getItem("currencyValue");
+        let conversions = JSON.parse(conversion);
+        console.log(conversions)
+        let UsCurrency = giveVal/conversions[fromCurrency];
+        toVal.value = UsCurrency*conversions[toCurrency];   
     }
+
 }
